fa25284b11b756cd4e8854be5b9796f154bce4db
Lorenzo Lizano Alpizar <lorenzolizanocr@gmail.com>
2022-02-07 20:22:51 -0600

    workshop completed

---
+++ src/components/TableBody.js
@@ -0,0 +1,24 @@
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Carlos</td>
+                <td>Ingeniero</td>
+            </tr>
+            <tr>
+                <td>Fatima</td>
+                <td>Educador</td>
+            </tr>
+            <tr>
+                <td>Lorenzo</td>
+                <td>Administrador</td>
+            </tr>
+            <tr>
+                <td>Jose</td>
+                <td>Arquitecto</td>
+            </tr>
+        </tbody>
+    );
+}
+
+export default TableBody;
